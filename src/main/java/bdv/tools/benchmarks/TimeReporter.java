eac1a51ffeadb0d667eeb5902a208d9ae48b4289
Vladimír Ulman <ulman@fi.muni.cz>
2024-07-10 11:52:02 +0200

    CHG: TimeReporter cleaner and sleeker, and probably FINAL :-)

--- src/main/java/bdv/tools/benchmarks/TimeReporter.java
+++ src/main/java/bdv/tools/benchmarks/TimeReporter.java
@@ -1,8 +1,5 @@
 package bdv.tools.benchmarks;
 
-import java.util.List;
-import java.util.ArrayList;
-
 public class TimeReporter {
 
 	private TimeReporter() {}
@@ -15,34 +12,37 @@ public class TimeReporter {
 		return INSTANCE;
 	}
 
-	private final List<Long> collectedTimes = new ArrayList<>(100);
-	private final List<String> collectedNames = new ArrayList<>(100);
 	private long initTime = -1;
-	private int wantedReportsNumber = -1;
+	private long lastReportTime = -1;
+	private int maxReportsNumber = -1;
 
-	public void startNowAndReportAfter(int wantedReportsNumber) {
-		this.wantedReportsNumber = wantedReportsNumber;
-		this.collectedTimes.clear();
-		this.collectedNames.clear();
+	/**
+	 * This method is intended for a (one) caller that starts some action and waits for
+	 * a number 'wantedReportsNumber' of workers, that got triggered by this action,
+	 * until they report themselves via {@link TimeReporter#reportWorkFinished(String)}.
+	 * @param wantedReportsNumber Number of measurements to appear on the console.
+	 */
+	public void startNowAndReportNotMoreThan(int wantedReportsNumber) {
+		this.maxReportsNumber = wantedReportsNumber;
 		this.initTime = System.currentTimeMillis();
+		this.lastReportTime = this.initTime;
 	}
 
+	/**
+	 * This is intended for workers (see {@link TimeReporter#startNowAndReportNotMoreThan(int)})
+	 * to report autonomously and immediately after they finished their current work. The method
+	 * is monitoring not to display more than a wanted number of reports (see again the above method).
+	 * @param callerID Identification of the worker to appear on the console.
+	 */
 	synchronized
 	public void reportWorkFinished(final String callerID) {
-		if (this.collectedTimes.size() < this.wantedReportsNumber) {
-			this.collectedTimes.add( System.currentTimeMillis() );
-			this.collectedNames.add( callerID );
-		}
+		if (this.maxReportsNumber > 0) {
+			maxReportsNumber -= 1;
 
-		if (this.collectedTimes.size() == this.wantedReportsNumber) {
-			long prevTime = initTime;
-			for (int i = 0; i < wantedReportsNumber; ++i) {
-				long time = collectedTimes.get(i);
-				System.out.println(collectedNames.get(i)+": Delay from first "+(double)(time-initTime)/1000.0
-						  +" seconds; from previous "+(double)(time-prevTime)/1000.0+" seconds");
-				prevTime = time;
-			}
-			this.wantedReportsNumber = -1;
+			long time = System.currentTimeMillis();
+			System.out.println(callerID+": Delay from init "+(double)(time-initTime)/1000.0
+					+" seconds; from previous "+(double)(time-lastReportTime)/1000.0+" seconds");
+			lastReportTime = time;
 		}
 	}
 }
